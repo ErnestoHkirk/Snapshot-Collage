26abab5869140cec8a912f1d39ae4e187c9ea6c8
ErnestoHKirk <hooghkirk.e@gmail.com>
2022-04-21 15:46:11 -0700

    made change

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 //   res.send("Hello World!");
 // });
 import path from "path";
+const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*"),
