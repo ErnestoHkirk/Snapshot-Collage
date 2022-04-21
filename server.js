a2807af286c7c77a17bff3a310b0d94b659f8fa3
ErnestoHKirk <hooghkirk.e@gmail.com>
2022-04-21 14:57:13 -0700

    Change port on sever.js for heroku env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import express from "express";
 import fetch from "node-fetch";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
