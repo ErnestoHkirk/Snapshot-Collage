e2348d8f079640875e2fe4e0aecfe500fc0dd3ed
ErnestoHKirk <hooghkirk.e@gmail.com>
2022-04-21 21:51:12 -0700

    fix routes on refresh

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import path from "path";
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  //app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("/"),
     (req, res) => {
       console.log("TEST1");
