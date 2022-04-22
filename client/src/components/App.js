de0540f8f8c6356b97cfdc27beb9040acbb17c12
ErnestoHKirk <hooghkirk.e@gmail.com>
2022-04-21 22:15:13 -0700

    fix routes on refresh

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import DisplayPage from "./pages/DisplayPage";
 import ErrorPage from "./pages/ErrorPage";
 import Header from "./Header";
