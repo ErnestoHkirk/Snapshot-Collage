c4f3f604cc49f980aa0a154834fd9b3fb9999dd8
ErnestoHKirk <hooghkirk.e@gmail.com>
2022-04-21 19:48:56 -0700

    fix routes on refresh

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("/*"),
+  app.get("*"),
     (req, res) => {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     };
@@ -46,5 +46,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-//to run: node server.js
-//to run better: nodemon server.js
+// to run: node server.js
+// to run better: nodemon server.js
+// heroku logs --tail
